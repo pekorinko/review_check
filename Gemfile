4b778733fcac4fa859bddf9061ca3a7e36a0e4ff
pekorinko <doridori.program@gmail.com>
2021-03-09 22:29:23 +0900

    ViewでURLを入力しても渡らない

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'selenium-webdriver'
 # Easy installation and use of web drivers to run system tests with browsers
 gem 'webdrivers'
 
+gem 'pry-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,11 +4,16 @@ class ResultsController < ApplicationController
   end
 
   def new
+    puts 'new'
+    @place = MyTools::PlaceDataScraper.new(@url)
   end
 
   def create
-    # url = params[:url]
-    # @place = MyTools::PlaceDataScraper.new(url).scrape
+    puts 'create'
+    url = params[:url]
+    puts url
+    url = 'https://www.google.com/search?q=%E6%B9%AF%E6%A5%BD%E3%81%AE%E9%87%8C+%E5%9B%BD%E7%AB%8B&rlz=1C5CHFA_enJP939JP939&oq=%E6%B9%AF%E6%A5%BD%E3%81%AE%E9%87%8C%E3%80%80%E5%9B%BD%E7%AB%8B&aqs=chrome..69i57.4119j0j7&sourceid=chrome&ie=UTF-8#lrd=0x6018e3951c8fde6f:0x429b84dcae3b48f6,1,,,'
+    @place = MyTools::PlaceDataScraper.new(url).scrape
   end
 
 end
