adaf5b7392324faa28a9c1da8c3e85a8ee9330cb
pekorinko <doridori.program@gmail.com>
2021-03-10 21:42:12 +0900

    NOT NULL制約追加

--- db/migrate/20210302124316_create_reviews.rb
+++ db/migrate/20210302124316_create_reviews.rb
@@ -1,9 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.string  :text
-      t.integer :count
-      t.float :star
+      t.string  :text, null: false
+      t.integer :count, null: false
+      t.float :star, null: false
       t.timestamps
     end
   end
