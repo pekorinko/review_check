fbc79ee6b2ffaa6206704738de49505e941aa89f
pekorinko <doridori.program@gmail.com>
2021-03-10 22:57:28 +0900

    インデントの修正

--- lib/my_tools/place_data_scraper.rb
+++ lib/my_tools/place_data_scraper.rb
@@ -1,20 +1,19 @@
 module MyTools
-    class PlaceDataScraper
+  class PlaceDataScraper
+    attr_reader :url
 
-        attr_reader :url
-        def initialize(url)
-            @url = url
-        end
-
-        def scrape
-            # Seleniumの処理
-            scrape_process = MyTools::SeleniumTool.new(@url)
-            results = scrape_process.access_url
-            results.each do |result|
-                Review.create(text: result[:text], count: result[:count], star: result[:star])
-            end
-            Review.find(1)
-        end    
-
+    def initialize(url)
+      @url = url
     end
+
+    def scrape
+      # Seleniumの処理
+      scrape_process = MyTools::SeleniumTool.new(@url)
+      results = scrape_process.access_url
+      results.each do |result|
+        Review.create(text: result[:text], count: result[:count], star: result[:star])
+      end
+      Review.find(1)
+    end
+  end
 end
--- lib/my_tools/selenium_tool.rb
+++ lib/my_tools/selenium_tool.rb
@@ -1,73 +1,84 @@
 module MyTools
-    class SeleniumTool
-        def initialize(url)
-            @url = url
-        end
-
-        def get_review_count(local_guide)
-            if local_guide.find_elements(:class_name, 'A503be').size > 0
-                local_guide_info = local_guide.find_element(:class_name, 'A503be')
-                result = local_guide_info.text.match(/(\d+)\ 件のレビュー/)
-                if result.nil?
-                    return 1
-                else
-                    result[0].to_i
-                end
-            else
-                return local_guide.text.scan(/(\d+)\ 件のレビュー/)[0].to_i
-            end
-        end
-
-        def access_url  
-            d = Selenium::WebDriver.for :chrome
-            
-            wait = Selenium::WebDriver::Wait.new(timeout: 30)
-
-            d.get(@url)
-            
-            wait.until { d.find_element(:class_name, 'lcorif').displayed? }
-            
-            #スクロールをループで書く
-            current_height = d.execute_script('return document.getElementsByClassName("review-dialog-list")[0].scrollHeight')
-            elements = []
-            
-            while true do
-                elements = d.find_elements(:css, '.gws-localreviews__google-review.WMbnJf')
-                if elements.length >= 30
-                    break
-                end
-                d.execute_script("document.getElementsByClassName('review-dialog-list')[0].scrollTo(0,#{current_height})")
-                sleep 5
-                current_height = d.execute_script('return document.getElementsByClassName("review-dialog-list")[0].scrollHeight')
-                sleep 5
-            end
-            
-            puts elements.length
-            
-            results = []
-        
-            elements.each do |element|
-                review_item = element.find_element(:class_name, 'Jtu6Td')
-                local_guide = element.find_element(:class_name, 'FGlxyd')
-                star_score = element.find_element(:class_name,"PuaHbe").find_element(:class_name,"Fam1ne").attribute('aria-label').scan(/\d+\.\d+?/)[0].to_f
-                
-                begin
-                    review_item.find_element(:css, '.review-more-link').click
-                    sleep 0.5
-                    content = review_item.find_element(:class_name, 'review-full-text')
-                    review_count = get_review_count(local_guide)
-                    hash = { text: content.text, star: star_score, count: review_count}
-                    results.push(hash)
-
-                rescue StandardError
-                    content = review_item.find_elements(:tag_name, 'span').last
-                    review_count = get_review_count(local_guide)
-                    hash = { text: content.text, count: review_count, star: star_score}
-                    results.push(hash)
-                end
-            end
-        end
+  class SeleniumTool
+    def initialize(url)
+      @url = url
     end
+
+    def get_review_count(local_guide)
+      if local_guide.find_elements(:class_name, 'A503be').size > 0
+        local_guide_info = local_guide.find_element(:class_name, 'A503be')
+        result = local_guide_info.text.match(/(\d+)\ 件のレビュー/)
+        if result.nil?
+          return 1
+        else
+          result[0].to_i
+        end
+      else
+        return local_guide.text.scan(/(\d+)\ 件のレビュー/)[0].to_i
+      end
+    end
+
+    def access_url
+      d = Selenium::WebDriver.for :chrome
+
+      wait = Selenium::WebDriver::Wait.new(timeout: 30)
+
+      d.get(@url)
+
+      wait.until { d.find_element(:class_name, 'lcorif').displayed? }
+
+      #スクロールをループで書く
+      current_height =
+        d.execute_script(
+          'return document.getElementsByClassName("review-dialog-list")[0].scrollHeight',
+        )
+      elements = []
+
+      while true
+        elements =
+          d.find_elements(:css, '.gws-localreviews__google-review.WMbnJf')
+        break if elements.length >= 30
+        d.execute_script(
+          "document.getElementsByClassName('review-dialog-list')[0].scrollTo(0,#{current_height})",
+        )
+        sleep 5
+        current_height =
+          d.execute_script(
+            'return document.getElementsByClassName("review-dialog-list")[0].scrollHeight',
+          )
+        sleep 5
+      end
+
+      puts elements.length
+
+      results = []
+
+      elements.each do |element|
+        review_item = element.find_element(:class_name, 'Jtu6Td')
+        local_guide = element.find_element(:class_name, 'FGlxyd')
+        star_score =
+          element
+            .find_element(:class_name, 'PuaHbe')
+            .find_element(:class_name, 'Fam1ne')
+            .attribute('aria-label')
+            .scan(/\d+\.\d+?/)[
+            0
+          ].to_f
+
+        begin
+          review_item.find_element(:css, '.review-more-link').click
+          sleep 0.5
+          content = review_item.find_element(:class_name, 'review-full-text')
+          review_count = get_review_count(local_guide)
+          hash = { text: content.text, star: star_score, count: review_count }
+          results.push(hash)
+        rescue StandardError
+          content = review_item.find_elements(:tag_name, 'span').last
+          review_count = get_review_count(local_guide)
+          hash = { text: content.text, count: review_count, star: star_score }
+          results.push(hash)
+        end
+      end
+    end
+  end
 end
-
-
