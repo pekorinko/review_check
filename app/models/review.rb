b85ac8d1919c8e9690e5e25ae44f080590a8adfa
pekorinko <doridori.program@gmail.com>
2021-03-13 10:25:00 +0900

    app/models/review.rbのコンフリクト解消

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,7 @@
 class Review < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :place, optional: true
-=======
+
   validates :text, presence: true
   validates :count, presence: true
   validates :star, presence: true
->>>>>>> 6e23f5216bd5a541ac1a577253f9d340480c43b4
 end
