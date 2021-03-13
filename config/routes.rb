a6210fdf74c835b17ffa49da376dbf77667b6f54
pekorinko <doridori.program@gmail.com>
2021-03-13 10:23:53 +0900

    config/routes.rbのコンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,1 @@
-Rails.application.routes.draw do
-  resources :results
-<<<<<<< HEAD
-  # post 'results/new' => 'results#new'
-=======
->>>>>>> 6e23f5216bd5a541ac1a577253f9d340480c43b4
-end
+Rails.application.routes.draw { resources :results }
