f5d5cb01a06b4ca5872172a3246daff051a792ab
pekorinko <doridori.program@gmail.com>
2021-03-15 20:51:12 +0900

    不要なファイルの削除、不要なインスタンス変数への代入をやめた

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,11 +4,7 @@ class ResultsController < ApplicationController
   def create
     url = params[:url]
     place_data_scraper = MyTools::PlaceDataScraper.new(url)
-    @place = place_data_scraper.review_save
+    place_data_scraper.review_save
     place_data_scraper.place_save
-  end
-
-  def result_params
-    params.require(:result).permit(:url)
   end
 end
