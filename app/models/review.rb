1dd53fd90cf0743f12f5ecdf5f33cac99a10de4e
pekorinko <doridori.program@gmail.com>
2021-03-21 15:03:12 +0900

    textカラムのバリデーションを削除

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :place, optional: true
-
-  validates :text, presence: false
   validates :count, presence: true
   validates :star, presence: true
 end
