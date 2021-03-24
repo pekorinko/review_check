88e144274926ccd973671a92097a0c959e836611
pekorinko <doridori.program@gmail.com>
2021-03-24 23:04:34 +0900

    星評価の平均点を返して表示

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,9 +4,9 @@ class ResultsController < ApplicationController
   def create
     url = params[:url]
     place_data_scraper = MyTools::PlaceDataScraper.new(url)
-    place_id = place_data_scraper.save_place
-    place_data_scraper.save_review(place_id)
-    check_credibility = MyTools::CheckCredibility.new(place_id)
-    @result_credibility = check_credibility.credibility
+    @place_id = place_data_scraper.save_place
+    place_data_scraper.save_review(@place_id)
+    check_credibility = MyTools::CheckCredibility.new(@place_id)
+    @result = check_credibility.credibility
   end
 end
--- lib/my_tools/check_credibility.rb
+++ lib/my_tools/check_credibility.rb
@@ -3,11 +3,15 @@ module MyTools
     attr_reader :place_id
 
     def initialize(place_id)
-      @place = place_id
+      @place_id = place_id
     end
 
     def credibility
-      return '30%'
+      reviews = Review.where(place_id: @place_id)
+      stars = reviews.map { |review| review.star }
+      total = 0
+      stars.each { |star| total = total + star }
+      return total / stars.length
     end
   end
 end
