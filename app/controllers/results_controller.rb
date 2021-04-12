9565241f30b6852edaa1329a85ddd38cedb5c24e
pekorinko <doridori.program@gmail.com>
2021-04-12 18:01:18 +0900

    url_validatorでURI.parse()せずにGoogleのURLか判別・コントローラーにおけるurl_filterとurl_validator処理の簡易化

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,6 @@
 class ResultsController < ApplicationController
+  attr_reader :url
+
   def index
     redirect_to new_result_path
   end
@@ -6,23 +8,17 @@ class ResultsController < ApplicationController
   def new; end
 
   def create
-    url = params[:url]
-    url_filter = MyTools::UrlFilter.new(url)
-    url_validator = MyTools::UrlValidator.new(url)
+    @url = params[:url]
 
-    if url.include?('www.google.com')
-      url = url_filter.filter
-      place_data_scraper = MyTools::PlaceDataScraper.new(url)
-      place = place_data_scraper.save_place
-      place_data_scraper.save_review(place.id)
-      check_credibility = MyTools::CheckCredibility.new(place.id)
-      @result = check_credibility.credibility
+    if @url.include?('www.google.com')
+      url_filter = MyTools::UrlFilter.new(@url)
+      @url = url_filter.filter
     end
 
-    if url.exclude?('www.google.com') && url_validator.validate
-      # GoogleのURLで変な要素付きのURLでもurl_validatorでURI.parseしちゃうからお腹壊す
-      # もしhostがgoogleじゃなかった時だけ「url_validator.validate」を起動させたい
-      place_data_scraper = MyTools::PlaceDataScraper.new(url)
+    url_validator = MyTools::UrlValidator.new(@url)
+    if url_validator.validate
+      @url = url_validator.validate
+      place_data_scraper = MyTools::PlaceDataScraper.new(@url)
       place = place_data_scraper.save_place
       place_data_scraper.save_review(place.id)
       check_credibility = MyTools::CheckCredibility.new(place.id)
@@ -32,4 +28,34 @@ class ResultsController < ApplicationController
       render :new
     end
   end
+
+  # 上記のコードと下記、どちらが良いか迷っている↓
+  # def create
+  #   url = params[:url]
+  #   url_filter = MyTools::UrlFilter.new(url)
+  #   url_validator = MyTools::UrlValidator.new(url)
+
+  #   if url.include?('www.google.com')
+  #     url = url_filter.filter
+  #     place_data_scraper = MyTools::PlaceDataScraper.new(url)
+  #     place = place_data_scraper.save_place
+  #     place_data_scraper.save_review(place.id)
+  #     check_credibility = MyTools::CheckCredibility.new(place.id)
+  #     @result = check_credibility.credibility
+  #   end
+
+  #   if url.exclude?('www.google.com') && url_validator.validate
+  #     # GoogleのURLで変な要素付きのURLでもurl_validatorでURI.parseしちゃうからお腹壊す
+  #     # もしhostがgoogleじゃなかった時だけ「url_validator.validate」を起動させたい
+  #     place_data_scraper = MyTools::PlaceDataScraper.new(url)
+  #     place = place_data_scraper.save_place
+  #     place_data_scraper.save_review(place.id)
+  #     check_credibility = MyTools::CheckCredibility.new(place.id)
+  #     @result = check_credibility.credibility
+  #   elsif url.exclude?('www.google.com') && !url_validator.validate
+  #     flash.now[:alert] = 'URLが不正です'
+  #     render :new
+  #   end
+
+  # end
 end
--- lib/my_tools/url_validator.rb
+++ lib/my_tools/url_validator.rb
@@ -5,9 +5,15 @@ module MyTools
     end
 
     def validate
-      # unless @url.include?(';tbs:lrf:') || @url.include?('mv:[[')
-      uri = URI.parse(@url)
-      uri.host == 'www.google.com' ? true : false
+      # URI.parse(@url)時に変な要素が含まれていることがBadURLエラーの原因だった
+      # そのため、URI.parse(@url)しないでGoogleのURLか否か判断するコードに書き換えた
+      # uri = URI.parse(@url)
+      # uri.host == 'www.google.com' ? true : false
+      if @url.include?('www.google.com')
+        return @url
+      else
+        false
+      end
     end
   end
 end
