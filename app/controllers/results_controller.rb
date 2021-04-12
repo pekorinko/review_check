0d6c6c44b46600824a32edf497b39747f574b5ff
pekorinko <doridori.program@gmail.com>
2021-04-12 18:02:47 +0900

    不要なコメントの削除

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -28,34 +28,4 @@ class ResultsController < ApplicationController
       render :new
     end
   end
-
-  # 上記のコードと下記、どちらが良いか迷っている↓
-  # def create
-  #   url = params[:url]
-  #   url_filter = MyTools::UrlFilter.new(url)
-  #   url_validator = MyTools::UrlValidator.new(url)
-
-  #   if url.include?('www.google.com')
-  #     url = url_filter.filter
-  #     place_data_scraper = MyTools::PlaceDataScraper.new(url)
-  #     place = place_data_scraper.save_place
-  #     place_data_scraper.save_review(place.id)
-  #     check_credibility = MyTools::CheckCredibility.new(place.id)
-  #     @result = check_credibility.credibility
-  #   end
-
-  #   if url.exclude?('www.google.com') && url_validator.validate
-  #     # GoogleのURLで変な要素付きのURLでもurl_validatorでURI.parseしちゃうからお腹壊す
-  #     # もしhostがgoogleじゃなかった時だけ「url_validator.validate」を起動させたい
-  #     place_data_scraper = MyTools::PlaceDataScraper.new(url)
-  #     place = place_data_scraper.save_place
-  #     place_data_scraper.save_review(place.id)
-  #     check_credibility = MyTools::CheckCredibility.new(place.id)
-  #     @result = check_credibility.credibility
-  #   elsif url.exclude?('www.google.com') && !url_validator.validate
-  #     flash.now[:alert] = 'URLが不正です'
-  #     render :new
-  #   end
-
-  # end
 end
--- lib/my_tools/url_validator.rb
+++ lib/my_tools/url_validator.rb
@@ -5,10 +5,6 @@ module MyTools
     end
 
     def validate
-      # URI.parse(@url)時に変な要素が含まれていることがBadURLエラーの原因だった
-      # そのため、URI.parse(@url)しないでGoogleのURLか否か判断するコードに書き換えた
-      # uri = URI.parse(@url)
-      # uri.host == 'www.google.com' ? true : false
       if @url.include?('www.google.com')
         return @url
       else
