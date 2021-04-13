2698146c0e805e173a217df6b8038c923bd1fb7a
pekorinko <doridori.program@gmail.com>
2021-04-13 23:16:42 +0900

    不要なコメント削除

--- lib/my_tools/url_filter.rb
+++ lib/my_tools/url_filter.rb
@@ -14,9 +14,6 @@ module MyTools
       elsif @url.exclude?(';tbs:lrf:') || @url.exclude?('mv:[[')
         return @url
       end
-      # @url.split(';tbs:lrf:')[0] if @url.include?(';tbs:lrf:')
-      # @url.split('mv:[[')[0] if @url.include?('mv:[[')
-      # return @url if @url.exclude?(';tbs:lrf:') || @url.exclude?('mv:[[')
     end
   end
 end
