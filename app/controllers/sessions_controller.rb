c3178769cebe474b341801afba705bbd1b5a8996
pekorinko <doridori.program@gmail.com>
2021-04-25 18:29:56 +0900

    Sessionsコントローラーに承認後の処理追加, UserモデルにUserレコード検索・作成する処理追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,5 @@
 class SessionsController < ApplicationController
+  user = User.find_or_create_from_auth_hash!(request.env['omniauth.auth'])
+  session[:user_id] = user.id
+  redirect_to root_path, notice: 'ログインしました'
 end
