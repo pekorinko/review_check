aaa622bcf473ed3f040aa4f02a3be5d4a1d4c370
pekorinko <doridori.program@gmail.com>
2021-04-27 22:40:53 +0900

    ログアウト用ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails
   .draw do
     root 'results#new'
     get '/auth/:provider/callback' => 'sessions#create'
+    delete '/logout' => 'sessions#destroy'
     resources :results, only: %i[new create index show]
   end
