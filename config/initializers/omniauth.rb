53ef7a763e0c00bcdcff207e5afbe4de1d8b9115
pekorinko <doridori.program@gmail.com>
2021-05-02 22:03:12 +0900

    暗号化して保存

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :github,
-             'ec3e97f5ec1b11a3f089',
-             '9bcb9d5e1e2079bd8fa8c17e0e45db59a39874a9'
+             Rails.application.credentials.github[:client_id_dev],
+             Rails.application.credentials.github[:client_secret_dev]
   else
     provider :github,
              Rails.application.credentials.github[:client_id],
