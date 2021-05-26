3842855357bdd96a4efb2dc655833f3b37c4ac06
pekorinko <doridori.program@gmail.com>
2021-05-27 08:51:16 +0900

    （WIP）Googleの口コミ無いURLが入力された際にbegin~rescueでターミナルに口コミ関連のURLではありませんと表示

--- lib/my_tools/place_data_scraper.rb
+++ lib/my_tools/place_data_scraper.rb
@@ -24,7 +24,11 @@ module MyTools
 
     def save_place
       uri = URI.parse(@url)
-      @lrd = uri.fragment.split('=')[1].split(',')[0]
+      begin
+        @lrd = uri.fragment.split('=')[1].split(',')[0]
+      rescue StandardError
+        puts '口コミ関連のURLではありません'
+      end
       place = Place.find_by(lrd: @lrd)
       @result = @scrape_process.fetch_place
 
