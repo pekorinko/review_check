2cbb6b6c3b3bf6257f2a1c1a68071c6e629457bd
pekorinko <doridori.program@gmail.com>
2021-07-07 18:11:41 +0900

    不要なファイルを作成しない設定

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module ReviewCheck
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.paths.add 'lib', eager_load: true
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+
   end
 end
